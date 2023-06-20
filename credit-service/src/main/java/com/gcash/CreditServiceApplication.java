516554d37dc01c2503116716736f3e53481f2f7a
jd <jeanjade.deguia@gcash.com>
2023-06-20 10:22:52 +0800

    added credit-service springboot

---
+++ credit-service/src/main/java/com/gcash/CreditServiceApplication.java
@@ -0,0 +1,22 @@
+package com.gcash;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CreditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner helloWorld() {
+		return args -> System.out.println("Hello World!");
+	}
+
+
+
+}
