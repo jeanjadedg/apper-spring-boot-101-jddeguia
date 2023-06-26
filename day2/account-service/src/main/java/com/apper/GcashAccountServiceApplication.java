9996ce1f0461804fb3af410e7b944cc9129b8224
jd <jeanjade.deguia@gcash.com>
2023-06-26 09:53:44 +0800

    created day2 folder

---
+++ day2/account-service/src/main/java/com/apper/GcashAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcashAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcashAccountServiceApplication.class, args);
+	}
+
+}
