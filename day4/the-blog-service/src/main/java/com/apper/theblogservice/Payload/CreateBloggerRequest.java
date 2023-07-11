26a30e33917d469f2899cb443d5eb7015122bd88
jeanjadedg <jeanjade.deguia@gcash.com>
2023-07-11 10:08:37 +0800

    created blogger service

---
+++ day4/the-blog-service/src/main/java/com/apper/theblogservice/Payload/CreateBloggerRequest.java
@@ -0,0 +1,25 @@
+package com.apper.theblogservice.Payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CreateBloggerRequest {
+
+    @JsonProperty("name")
+    @NotBlank(message = "Name is required.")
+    private String name;
+
+    @JsonProperty("email")
+    @Email
+    @NotBlank(message = "Email is required.")
+    private String email;
+
+    @Size(min = 5, message = "Password must be at least 5 characters.")
+    @NotBlank(message = "Password is required.")
+    private String password;
+
+}
