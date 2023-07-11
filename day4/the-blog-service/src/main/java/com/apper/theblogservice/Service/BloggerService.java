29b7dc6259c300ab9d0fd5fdc478f5b2fb89bbe2
jeanjadedg <jeanjade.deguia@gcash.com>
2023-07-11 10:24:14 +0800

    created service package

---
+++ day4/the-blog-service/src/main/java/com/apper/theblogservice/Service/BloggerService.java
@@ -0,0 +1,11 @@
+package com.apper.theblogservice.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BloggerService {
+
+    public Blogger createBlogger(String name, String email, String password) {
+
+    }
+}
